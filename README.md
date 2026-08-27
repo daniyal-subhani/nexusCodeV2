322ec40a6abce1266b39c57c01665d54adbbeae0
Daniyal Subhani <daniyalsubhani527@gmail.com>
2026-08-27 05:52:59 +0500

    chore(husky): setup pre-commit and commitlint hooks

---
+++ README.md
@@ -0,0 +1,1 @@
+# Version 2nd of NexusCore MonoRepo-Project
---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/build/**',
+      '**/coverage/**',
+    ],
+  },
+  eslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/.next/**'],
+  },
+];
